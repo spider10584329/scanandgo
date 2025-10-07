8c7a29bbab8b5cacd6c1ea39ae7e50e6e2266c07
spider10584329 <spider10584329@gmail.com>
2025-10-07 18:14:49 +0200

    fixed issue login disabled

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -1,8 +1,7 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
 import Image from 'next/image'
 
 interface SidebarProps {
